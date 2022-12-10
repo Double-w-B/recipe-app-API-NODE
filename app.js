309fadf6d8c7b5ac0af25ca7916e1e022fc0081f
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-12-10 23:22:13 +0100

    created authorization logic

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const cookieParser = require("cookie-parser");
+
 const connectDB = require("./db/connect");
+
+const authRouter = require("./routes/authRoutes");
 
 // middleware
 const notFoundMiddleware = require("./middleware/notFound");
 const errorHandlerMiddleware = require("./middleware/errorHandler");
 
-app.use("/", (req, res) => {
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.get("/", (req, res) => {
   res.send("Foodie mood API");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);

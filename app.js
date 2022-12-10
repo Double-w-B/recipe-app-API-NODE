9f70d461faf02b0a44ed20a815074f47dcf7af7a
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-12-10 19:04:12 +0100

    connected project to a MongoDB

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+
 app.use("/", (req, res) => {
   res.send("Foodie mood API");
 });
 
 const port = process.env.PORT || 5000;
 
-const startServer = async (req, res) => {
+const startServer = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port} ...`)
     );

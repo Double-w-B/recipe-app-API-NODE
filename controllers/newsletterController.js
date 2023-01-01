589e471843bd6c99a57bed65d898ef11f9e4b8a5
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2023-01-01 22:07:12 +0100

    added a route to verify the email address in a newsletter database

--- controllers/newsletterController.js
+++ controllers/newsletterController.js
@@ -2,6 +2,22 @@ const Newsletter = require("../models/Newsletter");
 const CustomError = require("../customErrors");
 const { StatusCodes } = require("http-status-codes");
 
+const getSingleEmail = async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    throw new CustomError.BadRequestError("Please provide email");
+  }
+
+  const userEmail = await Newsletter.findOne({ email });
+
+  if (!userEmail) {
+    throw new CustomError.NotFoundError("There is no such email in DB");
+  }
+
+  res.status(StatusCodes.OK).json({ email });
+};
+
 const addEmailToDB = async (req, res) => {
   const { email } = req.body;
 
@@ -38,4 +54,4 @@ const removeEmailFromDB = async (req, res) => {
   res.status(StatusCodes.OK).json({ removedEmail });
 };
 
-module.exports = { addEmailToDB, removeEmailFromDB };
+module.exports = { addEmailToDB, removeEmailFromDB, getSingleEmail };
--- routes/newsletterRoutes.js
+++ routes/newsletterRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
   addEmailToDB,
   removeEmailFromDB,
+  getSingleEmail,
 } = require("../controllers/newsletterController");
 
+router.post("/checkEmail", getSingleEmail);
 router.post("/add", addEmailToDB);
 router.post("/remove", removeEmailFromDB);
 
